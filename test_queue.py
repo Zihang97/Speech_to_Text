c6954e3cde4c33c2ce318adfba5a2313f93d5fb6
Zihang97 <jzh15@bu.edu>
2022-03-22 14:55:08 -0400

    Add files via upload

--- test_queue.py
+++ test_queue.py
@@ -2,10 +2,25 @@ import pytest
 from queue_stub import *
 from queue_speech_to_text import *
 
-def test_stub_func():
+def test_stub_func_without_input():
 	with pytest.raises(TypeError):
 		stub_func()
 
+def test_stub_func_with_input2():
+	start = time.time()
+	q = multiprocessing.Queue()
+	q.put(2)
+	stub_func(q)
+	end = time.time()
+	assert end - start >= 2
+
+def test_stub_func_with_input3():
+	start = time.time()
+	q = multiprocessing.Queue()
+	q.put(3)
+	stub_func(q)
+	end = time.time()
+	assert end - start >= 3
 
 def test_speech_func():
 	with pytest.raises(TypeError):
